ef82111639780ed49a101bde2c2d97248fbaf371
Memmed <memmed_memmedli@hotmail.com>
2016-08-28 13:37:09 -0700

    asd

---
+++ index.php
@@ -0,0 +1,62 @@
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="pic.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="file">
+		<input type="submit">
+	</form>
+</body> -->
+<!-- </html> -->
+<?php 
+	 // $i = 016;
+  //    echo $i / 2;
+	// $a = 45;
+	// $b = 12;
+	// echo $a + $b;
+	// $my_array = array('me', 'you' , 'it');
+	// $newArray = serialize($my_array);
+
+	// print_r($newArray);
+	// $var = 'me';
+	// $$var = 4;
+	// echo $me;
+		// $a = 5;
+		// function foo(&$var){
+	 //  	  $var++;
+	 //    	echo $var;
+		// }
+		// function &bar(){
+	 //   		return $a;
+		// }
+		//  $foo1 = foo(bar());
+		// import:
+ 		// $x = ;
+   //     	var_dump($x);
+	// class data_base{
+	// 	private $connection;
+	// 	private $host;
+	// 	private $username;
+	// 	private $password;
+	// 	private $database;
+
+	// 	private function __construct(){
+	// 		$this -> connection = new sql($this->host, $this->username, $this->password, $this->database);
+	// 	} 
+	// }
+ //    trait my_trait{
+ //    	public $name;
+ //    	public function yaz(){
+ //    		echo $this->name;
+ //    	}
+ //    }
+ //    class my_class{
+ //    	use my_trait;
+ //    }
+
+ //    $new_user = new my_class();
+ //    $new_user -> name = 'Memo';
+ //    $new_user -> yaz();
+ // ?>
---
+++ pic.php
@@ -0,0 +1,12 @@
+<?php 
+	
+	// $my_pic = $_FILES['file']['name'];
+	// $info = pathinfo($my_pic, PATHINFO_EXTENSION);
+
+	// if($info == "jpg" || $info == "png" || $info == "gif"){
+	// 	echo $info;	
+	// }else{
+	// 	echo "CHOOSE A JPG, PNG or GIF FILE PLEASE!!!";
+	// }
+
+?>
